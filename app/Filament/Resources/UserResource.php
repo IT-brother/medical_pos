fff1d6d69f07beff4c9642ebece7026d6e20a0f2
KoSoe-52 <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>
2025-05-30 09:55:18 +0630

    TEst

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -19,7 +19,7 @@ class UserResource extends Resource
 
     protected static ?string $navigationIcon = 'heroicon-o-user-plus';
     protected static ?string $navigationGroup = 'Settings';
-
+  // hello
     public static function form(Form $form): Form
     {
         return $form
