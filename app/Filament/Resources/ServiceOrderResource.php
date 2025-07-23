6703150e69050a6aa0672935acb114a61f494bed
KoSoe-52 <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>
2025-07-23 12:01:40 +0630

    Change Voucher Code in Edit Form

--- app/Filament/Resources/ServiceOrderResource.php
+++ app/Filament/Resources/ServiceOrderResource.php
@@ -34,8 +34,8 @@ class ServiceOrderResource extends Resource
                     ->required()
                     ->numeric()
                     ->default(0),
-                // Forms\Components\DatePicker::make('date')
-                //     ->required(),
+                Forms\Components\DatePicker::make('date')
+                    ->required(),
                 Forms\Components\TextInput::make('patient')
                     ->required()
                     ->maxLength(255),
