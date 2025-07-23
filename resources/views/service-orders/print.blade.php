259e279a6a817caf3384a60582d74dd5165d364f
KoSoe-52 <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>
2025-07-23 12:15:28 +0630

    Updated At in Print

--- resources/views/service-orders/print.blade.php
+++ resources/views/service-orders/print.blade.php
@@ -64,7 +64,7 @@
             <table>
                 <tr>
                     <td style="padding:8px">Voucher No: {{$order->voucher_no }}</td>
-                    <td style="padding:8px">Date: {{ date('d-m-Y H:i',strtotime($order->created_at)) }}</td>
+                    <td style="padding:8px">Date: {{ date('d-m-Y',strtotime($order->updated_at)) }}</td>
                 </tr>
                 <tr>
                     <td style="padding:8px">Customer: {{ $order->patient }}</td>
